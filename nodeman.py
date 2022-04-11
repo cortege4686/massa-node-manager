8fb800f5738862034d359f92b47fef576616d2f0
cortege3686 <rodesofatos@gmail.com>
2022-04-11 06:47:03 -0400

    q

--- nodeman.py
+++ nodeman.py
@@ -1,4 +1,4 @@
-import sys,time
+import time, subprocess as sp
 
 
 def write_log(msg):
@@ -6,10 +6,14 @@ def write_log(msg):
     log_file.write(f'{msg} \n')
     log_file.close
 
+def node_get_status():
+    node_stat = str(sp.check_output(['bash', 'shelall/get-status.sh']))
+    return node_stat
 
 def main():
     while 1:
         write_log('workornot')
+        print(node_get_status())
         time.sleep(5)
 
 
