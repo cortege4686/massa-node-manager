ecfed17a4b51406ad03c00b75e3decfd7fe1f3bc
cortege4686 <rodesofatos@gmail.com>
2022-04-15 18:04:54 +0000

    r

---
+++ conf.py
@@ -0,0 +1,18 @@
+import subprocess as sp
+
+conf_main_sleep_time = 60
+
+
+def conf_node_get_status():
+    node_stat = str(sp.check_output(['bash', 'shelall/get-status.sh']))
+
+    if 'Network stats:' in node_stat:
+        # 1 MEANS GREEN
+        return 1, node_stat 
+    else:
+        # 0 = RED
+        return 0, node_stat, ''
+
+
+
+
---
+++ nodeman.py
@@ -0,0 +1,53 @@
+import json, time, re
+from conf import conf_main_sleep_time
+##########################
+#  --------------------
+##########################
+
+
+def write_log(msg):
+    log_file = open('logs/nodeman-hidden.logs', 'a')
+    log_file.write(f'{msg}\n')
+    log_file.close()
+    
+
+def restart_node(inp):
+    # RESTART LOOP
+    write_log(inp)
+
+
+def parser_nodov(connected):
+
+    def save_json(inp):
+        def cleaned():
+            node_id = re.findall(r'Node\'s ID: (.*?) / IP', inp.split('Connected nodes:')[1])
+            node_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', inp.split('Connected nodes:')[1])
+            return dict(zip(node_id, node_ip))
+
+        with open("logs/connections.json", "r") as lc_read:
+            from_file = dict(json.load(lc_read))
+        with open("logs/connections.json", "w") as lc_write:
+            lc_write.write(json.dumps(cleaned() | from_file))
+
+    save_json(connected)
+
+
+def node_feel_good():
+    from conf import conf_node_get_status
+
+    led, status = conf_node_get_status()
+    if led == 1:
+        parser_nodov(status)
+    else:
+        write_log('restart attempt')
+        restart_node(status)
+
+
+def main():
+    while 1:
+        node_feel_good()
+        time.sleep(conf_main_sleep_time)
+
+
+if __name__ == '__main__':
+    main()
