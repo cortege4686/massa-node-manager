8a942d7f136fe87a6c9ad4e06588e54765439a38
cortege3686 <rodesofatos@gmail.com>
2022-04-16 10:24:25 -0400

    fixxxox

--- shelall/restart-node.sh
+++ shelall/restart-node.sh
@@ -1,4 +1,2 @@
 pkill massa-node
-screen -XS mas_auto quit
-screen -dmS mas_auto
 screen -S mas_auto -X stuff "cd ~/massa/massa-node/ && RUST_BACKTRACE=full cargo run --release |& tee ~/nodeman-massa/logs/massa-hidden.logs"$(echo -ne '\015')
